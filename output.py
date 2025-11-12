ddcb3750b1f751bb34b159655a024c77ab03efde
Ziad Elshereif <ziadkhaled1918@gmail.com>
2025-11-12 16:01:34 +0100

    Refactor the code (step 2)

---
+++ output.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+
+def create_plot(fig, subplot_num, u, time_ms, T_cold, T_hot):
+    ax = fig.add_subplot(subplot_num)
+    im = ax.imshow(u.copy(), cmap=plt.get_cmap('hot'), vmin=T_cold, vmax=T_hot)
+    ax.set_axis_off()
+    ax.set_title('{:.1f} ms'.format(time_ms))
+    return im
+
+
+def output_plots(fig, im):
+    fig.subplots_adjust(right=0.85)
+    cbar_ax = fig.add_axes([0.9, 0.15, 0.03, 0.7])
+    cbar_ax.set_xlabel('$T$ / K', labelpad=20)
+    fig.colorbar(im, cax=cbar_ax)
+    plt.show()
